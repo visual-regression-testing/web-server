cfb9b788e760d84e91fd59cd93de9ab1f74546ab
Mike DiDomizio <mdidomizio@coveo.com>
2022-01-15 08:34:05 -0500

    Move S3 logic to function

---
+++ pages/api/upload.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import formidable from "formidable";
+
+import {PassThrough} from "stream";
+import AWS from 'aws-sdk';
+import {uploadToS3} from "../../shared/helpers/uploadToS3";
+
+// do not delete
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+async function postHandler(
+  req: NextApiRequest,
+  res: NextApiResponse<void>
+) {
+  console.log('request to upload image');
+
+  try {
+    await uploadToS3(req, 'visual-regression-testing-test');
+    res.status(201).send();
+  } catch(e) {
+    // todo
+  }
+
+
+  res.status(400).send();
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<void>
+) {
+  if(req.method === "POST") {
+    return postHandler(req, res);
+  }
+
+  console.log('unsupported http method');
+  res.status(405).send();
+};
---
+++ shared/helpers/uploadToS3.ts
@@ -0,0 +1,61 @@
+import AWS from "aws-sdk";
+import {PassThrough} from "stream";
+import formidable, {FileJSON} from "formidable";
+import {NextApiRequest} from "next";
+
+// todo can we do better than https://github.com/node-formidable/formidable/blob/master/examples/store-files-on-s3.js
+export function uploadToS3(req: NextApiRequest, bucket: string): Promise<void> {
+    if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+        throw new Error('AWS params not set');
+    }
+    // todo can we do better than https://github.com/node-formidable/formidable/blob/master/examples/store-files-on-s3.js
+    const s3Client = new AWS.S3();
+    const acceptableMimeTypes = ['image/jpeg', 'image/png'];
+
+    const uploadStream = (file: FileJSON) => {
+        if (!file.originalFilename) {
+            throw new Error('does not have original filename');
+        }
+
+        if (file.mimetype && acceptableMimeTypes.includes(file?.mimetype)) {
+            throw new Error('File does not have correct Mime Type')
+        }
+
+        const pass = new PassThrough();
+        s3Client.upload(
+            {
+                Bucket: bucket, // todo hardcode
+                Key: file.originalFilename, // file.newFilename for generated filename without extension
+                Body: pass,
+            },
+            (err: any, data: any) => {
+                if (err !== null) {
+                    // todo
+                    console.error('error occurred on push to S3');
+                }
+
+                if (data) {
+                    // todo success!
+                    console.log('successfully uploaded', data);
+                }
+            },
+        );
+
+        return pass;
+    };
+
+    return new Promise((resolve, reject) => {
+        const form = formidable({
+            fileWriteStreamHandler: uploadStream as any,
+        });
+
+        form.parse(req, (err, fields, files) => {
+            console.log('parsed', fields);
+            resolve();
+        });
+
+        // form.on('field', (fieldName, fieldValue) => {
+        //     console.log(fieldName, fieldValue);
+        // });
+    })
+}
