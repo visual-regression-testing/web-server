fc7143ef78b70f9089fc2e93170ab77e56f3e3b4
Mike DiDomizio <mikedidomizio@gmail.com>
2022-01-15 08:45:07 -0500

    Add ability to read S3 objects

---
+++ shared/helpers/readS3Files.ts
@@ -0,0 +1,24 @@
+import AWS from "aws-sdk";
+import {ListObjectsOutput} from "@aws-sdk/client-s3";
+
+export async function readS3Files(bucket: string, folder: string = ''): Promise<ListObjectsOutput> {
+    const s3 = new AWS.S3();
+
+    const params = {
+        Bucket: bucket,
+        Delimiter: '/' + folder,
+        // Prefix: 's/5469b2f5b4292d22522e84e0/ms.files/'
+    }
+
+    return new Promise((resolve, reject) => {
+        s3.listObjects(params, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+
+            if (data) {
+                resolve(data);
+            }
+        });
+    });
+}
