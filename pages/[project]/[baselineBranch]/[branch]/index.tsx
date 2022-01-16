27ae2b11f710ba46004de6b5d10602f8d3385f35
Mike DiDomizio <mikedidomizio@gmail.com>
2022-01-16 18:24:42 -0500

    Reads DB and files together

--- pages/[project]/[baselineBranch]/[branch]/index.tsx
+++ pages/[project]/[baselineBranch]/[branch]/index.tsx
@@ -1,39 +1,67 @@
-import {NextPage} from 'next';
+import {GetServerSideProps, NextPage} from 'next';
 import {useRouter} from 'next/router';
 import useSWR from 'swr';
-import {_Object, ListObjectsOutput} from "@aws-sdk/client-s3";
+import {ListObjectsOutput} from "@aws-sdk/client-s3";
 import config from "../../../../config/config";
+import {TestInformation} from "../../../api/read";
 
 // @ts-ignore
-const fetcher = (...args) => fetch(...args).then(res => res.json())
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+// https://stackoverflow.com/questions/68263146/next-js-doesnt-return-slug-with-userouter-in-console-log
+export const getServerSideProps: GetServerSideProps<any> = async (context) => {
+    let { project, baselineBranch, branch } = context.query;
+    // If slug is "undefined", since "undefined" cannot be serialized, server will throw error
+    // But null can be serializable
+    if (!project) {
+        // @ts-ignore
+        project = null;
+    }
+
+    if (!baselineBranch) {
+        // @ts-ignore
+        baselineBranch = null;
+    }
+
+    if (!branch) {
+        // @ts-ignore
+        branch = null;
+    }
+
+    // now we are passing the slug to the component
+    return { props: { project, baselineBranch, branch } };
+};
 
 const PullRequest: NextPage = () => {
-
     const bucket = config.bucket;
-    const router = useRouter()
+    const router = useRouter();
     const { project, baselineBranch, branch } = router.query as { project: string, baselineBranch: string, branch: string};
     const queryParams = new URLSearchParams({
         project: project,
         baselineBranch: baselineBranch,
-        // branch: branch
+        branch: branch
     });
 
-    const { data, error } = useSWR<ListObjectsOutput>(`/api/read?${queryParams}`, fetcher)
+    const { data, error } = useSWR<TestInformation[]>(`/api/read?${queryParams}`, fetcher)
 
     if (data) {
-        // todo this won't work with multiple sets of images
-        const sortedData = data?.Contents?.sort((a: any, b: any) => {
-            // the base image gets pushed all the way to the left
-            if (a.Key.match(/^[^\/]+\/[^\/]+\/([\w\-]+\.png)/)) {
-                return -1;
-            }
 
-            // the diff image gets pushed far right
-            if (a.Key.includes('diff')) {
-                return 1;
-            }
+        const sortedData: TestInformation[] = data.map(test => {
+            test.images.sort((a: string, b: string) => {
+               // the base image gets pushed all the way to the left
+               if (a.match(/^[^\/]+\/[^\/]+\/([\w\-]+\.png)/)) {
+                   return -1;
+               }
 
-            return 0;
+               // the diff image gets pushed far right
+               if (a.includes('diff')) {
+                   return 1;
+               }
+
+               return 0;
+           });
+
+            return test;
         });
 
         return (
@@ -41,7 +69,7 @@ const PullRequest: NextPage = () => {
                 <div>
                     <button>Approve</button>
                 </div>
-                {sortedData?.map(file => <img key={file.ETag} alt={file.Key} width="300" src={`https://${bucket}.s3.amazonaws.com/${file.Key}`}/>)}
+                {sortedData?.map((file, i) => file.images.map(image => <img key={i} alt={file.buildInformation.test_name} width="300" src={`https://${bucket}.s3.amazonaws.com/${image}`}/>))}
             </>
         )
     }
--- pages/api/read.ts
+++ pages/api/read.ts
@@ -2,6 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import {s3ListObjects} from "../../shared/helpers/s3ListObjects";
 import {ListObjectsOutput} from "@aws-sdk/client-s3";
 import config from '../../config/config';
+import { query } from '../../config/db'
+
+interface Test {
+    // project: string;
+    // baseline_branch: string;
+    // branch: string;
+    // build: string; // todo number
+    test_name: string;
+}
+
+export interface TestInformation {
+    buildInformation: Test,
+    images: [string, string, string];
+}
 
 const generateFolder = (project: string, baselineBranch: string): string => {
     return `${project}/${baselineBranch}`;
@@ -9,25 +23,64 @@ const generateFolder = (project: string, baselineBranch: string): string => {
 
 async function getHandler(
     req: NextApiRequest,
-    res: NextApiResponse<ListObjectsOutput | void>
+    res: NextApiResponse<TestInformation[] | void>
 ) {
     console.log('request to read images');
-
     const { project, baselineBranch, branch } = req.query as { project: string, baselineBranch: string, branch: string};
 
+    if (!project || !baselineBranch || !branch) {
+        res.status(400).send();
+    }
+
+    let results: Test[] = [];
+
     try {
-        const files = await s3ListObjects(config.bucket, generateFolder(project, baselineBranch));
-        res.status(200).send(files);
+        // todo I have no idea if this properly sanitizes
+        // todo it would be nice to make helper functions instead of just queries right here
+        results = await query<Test[]>(
+            `SELECT test_name FROM builds WHERE project = ? AND baseline_branch = ? AND branch = ?`,
+            [project, baselineBranch, branch]
+        )
     } catch(e) {
         // todo
     }
 
-    res.status(400).send();
+    let files: ListObjectsOutput = {};
+
+    try {
+         files = await s3ListObjects(config.bucket, generateFolder(project, baselineBranch));
+         files.Contents = (files as any).Contents.filter((object: any) => object.Key.charAt(object.Key.length - 1) !== '/');
+
+
+         console.log(files.Contents)
+    } catch(e) {
+        // todo
+    }
+
+
+    const testOutput: TestInformation[] = results.map(test => {
+        // todo this isn't the greatest way of handling it but it might work for a while
+        const images: string[] = (files as any).Contents.filter((object: any) => object.Key.match(/$/)).map((object: any) => object.Key);
+
+        console.log(images)
+        if (images.length > 3) {
+            // todo handle
+            console.error('bad')
+            res.status(500).send();
+        }
+
+        return {
+            buildInformation: test,
+            images: images as [string, string, string],
+        }
+    });
+
+    res.status(200).send(testOutput);
 }
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<ListObjectsOutput | void>
+    res: NextApiResponse<TestInformation[] | void>
 ) {
     if(req.method === "GET") {
         return getHandler(req, res);
