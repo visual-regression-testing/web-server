f95b2ade052669d3322bc7fff08f0a3f9cb28806
Mike DiDomizio <mikedidomizio@gmail.com>
2022-04-10 17:13:20 -0400

    Test for env in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+  },
   reactStrictMode: true,
 }
 
