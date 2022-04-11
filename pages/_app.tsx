b71e0ae4db579e4cdf8ffa66864afb9e6088c2b1
Mike DiDomizio <mikedidomizio@gmail.com>
2022-04-10 21:47:28 -0400

    Try with new Github client id/secret and amplify package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import { Amplify } from "aws-amplify";
 import { SessionProvider } from "next-auth/react"
+
+Amplify.configure({ ssr: true });
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
